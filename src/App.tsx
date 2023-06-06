e76d20928ffd56fbe6d7049941ac5eae919b88d6
Sanjay Darshan <sanjay.darshan@ecstvm.local>
2023-06-06 14:22:48 +0530

    Memory game app initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+// Define types and interfaces
+interface Card {
+  id: number;
+  value: string;
+  isFlipped: boolean;
+  isMatched: boolean;
+}
+
+function GameApp() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [selectedCard, setSelectedCard] = useState<Card | null>(null);
+
+  useEffect(() => {
+    // Generate initial cards
+    const initialCards: Card[] = [
+      { id: 1, value: "A", isFlipped: false, isMatched: false },
+      { id: 2, value: "A", isFlipped: false, isMatched: false },
+      { id: 3, value: "B", isFlipped: false, isMatched: false },
+      { id: 4, value: "B", isFlipped: false, isMatched: false },
+    ];
+
+    // Shuffle the cards
+    const shuffledCards = shuffleCards(initialCards);
+    setCards(shuffledCards);
+  }, []);
+
+  const handleCardClick = (card: Card) => {
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    if (selectedCard) {
+      // Check if the selected card matches the current card
+      if (selectedCard.value === card.value) {
+        setCards((prevCards) =>
+          prevCards.map((prevCard) =>
+            prevCard.id === selectedCard.id || prevCard.id === card.id
+              ? { ...prevCard, isMatched: true }
+              : prevCard
+          )
+        );
+      } else {
+        // Flip both cards back after a short delay
+        setTimeout(() => {
+          setCards((prevCards) =>
+            prevCards.map((prevCard) =>
+              prevCard.id === selectedCard.id || prevCard.id === card.id
+                ? { ...prevCard, isFlipped: false }
+                : prevCard
+            )
+          );
+        }, 1000);
+      }
+
+      setSelectedCard(null);
+    } else {
+      setSelectedCard(card);
+    }
+
+    setCards((prevCards) =>
+      prevCards.map((prevCard) =>
+        prevCard.id === card.id ? { ...prevCard, isFlipped: true } : prevCard
+      )
+    );
+  };
+
+  const shuffleCards = (cards: Card[]) => {
+    const shuffledCards = [...cards];
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledCards[i], shuffledCards[j]] = [
+        shuffledCards[j],
+        shuffledCards[i],
+      ];
+    }
+    return shuffledCards;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Memory Game</h1>
+      <div className="cards">
+        {cards.map((card) => (
+          <div
+            key={card.id}
+            className={`card ${card.isFlipped ? "flipped" : ""} ${
+              card.isMatched ? "matched" : ""
+            }`}
+            onClick={() => handleCardClick(card)}
+          >
+            {card.isFlipped && !card.isMatched ? card.value : ""}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default GameApp;
